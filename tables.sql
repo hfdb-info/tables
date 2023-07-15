d2cc30d155c2e6c51a8328347da4acef80ab8189
Justin Blalock <113410576+justin-blalock@users.noreply.github.com>
2023-07-15 09:09:24 -0500

    Update tables.sql
    Added latestprice view

--- tables.sql
+++ tables.sql
@@ -27,3 +27,9 @@ primary key (SKU, promoID, couponID),
 foreign key (SKU) references Products on delete cascade,
 check (SKU > 0 and price >= 0 and (promoID is not null) and (couponID is not null) and (ts is not null) and (expiration is not null))
 );
+
+create view latestprice as select retailprices.sku as sku,price,ts from (
+retailprices inner join (
+select sku, max(ts) as maxTS from retailprices group by sku
+) as latestPrice on latestprice.sku=retailprices.sku and ts=maxTS
+);
